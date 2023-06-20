65c8d413a57289ac3794a1e9ab9b3bfb0f729cb4
serah wanjiru <WanjiruMburuuu@gmail.com>
2023-06-20 08:53:08 +0300

    first commit

--- app/src/main/java/com/example/helloo/MainActivity.kt
+++ app/src/main/java/com/example/helloo/MainActivity.kt
@@ -1,12 +1,76 @@
-package com.example.helloo
 
+package com.example.hello
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.example.hello.dataBinding.ActivityMainBinding
+import com.example.helloo.LoginActivity
+import com.example.helloo.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root) //binding activity to one layout ie linking
+
+
+        //make sure you cast before onclick listener.
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        binding.tvLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnsignup.setOnClickListener {
+            validateSignUp()
+        }
+    }
+
+
+    fun validateSignUp() {
+        val firstName = binding.etFirstName.text.toString()
+        val lastName = binding.etLastName.text.toString()
+        val pass = binding.etPassword.text.toString()
+        val email = binding.etEmail.text.toString()
+        var error = false
+
+        if (firstName.isBlank()) {
+            binding.tilFirstName.error = "First name is required"
+            error = true
+        }
+        if (lastName.isBlank()) {
+            binding.tilLastName.error = "Last name is required"
+            error = true
+        }
+        if (email.isBlank()) {
+            binding.tilEmail.error = "Email is required"
+            error = true
+        }
+        if (pass.isBlank()) {
+            binding.tilPassword.error = "Password is required"
+            error = true
+        }
+
+        if (!error) {
+            Toast.makeText(this, "$firstName $lastName $email", Toast.LENGTH_LONG).show()
+
+        }
+
     }
 }
 
